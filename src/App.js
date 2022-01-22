f9a78f1754015899a3e20970de82da3f89aeba0e
Gliitni <linilorance298@gmail.com>
2022-01-22 16:27:45 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,35 +1,111 @@
-import { Route,Switch,Redirect } from 'react-router-dom';
+import { Route,Switch,Redirect,useHistory } from 'react-router-dom';
+import { useState } from 'react';
 import Welcome from './Welcome.js';
 import Login from './Login.js';
 import Products from './Products';
 
 import ProductDetails from './ProductDetails';
 
-function App() {
+const App =()=> {
+  
+  const [username, setUsername] =useState("");
+  const [password, setPassword]=useState("");
+  const [log, setLog] =useState();
+  console.log("log",log)
+
+  const history=useHistory();
+
+  const login =()=>{
+  console.log("up",username,password)
+  const requestOption = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify( {username: username,password:password})
+  };
+  fetch('https://chtimesheet.azurewebsites.net/api/Auth/login', requestOption)
+  .then((response) => {
+  
+      if (response.ok) {
+        return  (response.json()); 
+      }  else{
+       return response.json().then((data) => 
+        
+        { const error = 'login failed';
+           throw new Error(error);
+        });
+      }
+    })
+
+    .then((data) => {
+      console.log("data",data);
+      setLog(data);
+      localStorage.setItem('user',data.email);
+      localStorage.setItem('token',data.token);
+      history.push('/Welcome')  
+    })
+    .catch((err) => {
+      console.log("err",err)
+      alert(err.message);
+    });
+  }
+
+//   fetch('https://chtimesheet.azurewebsites.net/api/Auth/login', {
+//     method: 'post',
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8"
+//     },
+//     body:JSON.stringify(username,password)
+//   }).then(res =>{
+//     return res.json(); //error here
+//   }).then(data=>{
+//     console.log(data);
+//   }).catch((error) => {
+//     console.log(error);
+//  alert(error.message);
+//   })
+//   }
   return (
    
     <div>
-      {/* <MainHeader /> */}
+      
       <main>
       <Switch>
+      {/* {(log.token?log.username:'you are loged') */}
+        {/* { log.token &&( */}
+        
         <Route path='/' exact>
           <Redirect to='/Welcome' />
         </Route>
+        
+         {/* )} */}
+           
+         {/* {log.token!=""&&
+         (  */}
+      <Route path="/Login">
+      <Login login={login} setUsername={setUsername} setPassword={setPassword}/>
+    </Route>
+        {/* )}  */}
+         {/* {log.token&&( */}
+      
+        {/* { log.token=''&& */}
+        {/* (  */}
       <Route path="/welcome">
         <Welcome />
       </Route>
-      <Route path="/Login">
-        <Login />
-      </Route>
-
+ {/* )} */}
+        {/* )} */}
+      {/* {!log &&  ( */}
       <Route path="/products" exact>
         <Products />
       </Route>
+       {/* )}  */}
       <Route path="/Products/:ProductId">
         <ProductDetails />
       </Route>
+     
       </Switch>
       </main>
+     
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,109 +1,31 @@
-import { useState,useEffect } from "react"
-import { useHistory } from 'react-router-dom';
+import React from "react"
+
 import classes from './App.css';
 
 
-const Login=()=> {
-  const [username, setUsername] =useState("");
-  const [password, setPassword]=useState("");
-//   const [errorMessage, setErrorMessage] =useState();
+const Login=(prop)=> {
+  // console.log("prop",prop)
+  // const login =()=>{
+  // localStorage.getItem('user',log)
+ 
 
-
-  const history=useHistory();
-
-  useEffect(()=>{
-    if(localStorage.getItem('user-info')){
-   
-    history.push("/add");
-    
-  }
-    }, [])
-   
-
-  const login =(e)=>{
-    e.preventDefault();
-    
-    
-    console.log("up",username,password)
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username: username,password:password })
-    };
-    fetch('https://chtimesheet.azurewebsites.net/api/Auth/login', requestOptions)
-    //   .then(response => response.json())
-    //   .then(data => console.log('data',data))
-    
-        
-       
-           
-    //   .catch((err) => {
-    //     alert(err.message);
-    //   });
-
-    .then((response) => {
-        // setIsLoading(false);
-        if (response.ok) {
-          return response.json();
-        } 
-        else
-         {
-          return response.json().then((data) => 
-          {
-            const error = 'login failed';
-
-            throw new Error(error);
-          });
-        }
-      })
- // if (data!=="") {
-        //   history.push('/Welcome')
-        // } else 
-
-      .then((data) => {
-        console.log(data);
-        history.push('/Welcome')
-      })
-      
-      .catch((err) => {
-        alert(err.message);
-      });
-    }
-      
-    
-//     const result =  fetch("https://chtimesheet.azurewebsites.net/api/Auth/login",{
-//       method:'POST',
-//       headers: {'content-type': 'application/json'},
-//       body: JSON.stringify(
-//         { username: username,
-//           password:password 
-//       }),
-//     });
-
-//     const data = result.JSON()
-
-//   localStorage.setItem("user-info",result)
-//   console.log("data",data)
-//   }
   return (
 <>
-    
-    <form className="row">
+  <form className="row">
+  
         <h1>SIGN IN </h1>
+        {/* {prop.name} */}
     <div className="row">
       <label>Email:</label><br/>
-      <input text="email" placeholder="Enter email" onChange={(e)=>setUsername(e.target.value)}/><br/>
+      <input text="email" placeholder="Enter email" onChange={(e)=>prop.setUsername(e.target.value)}/><br/>
       <label >Password:</label><br/>
-      <input text="password" placeholder="Enter password" onChange={(e)=>setPassword(e.target.value)}/><br/>
+      <input text="password" placeholder="Enter password" onChange={(e)=>prop.setPassword(e.target.value)}/><br/>
       
-      <button className="button" onClick={login}>Login</button>
+      <button className="button" onClick={prop.login}>Login</button>
        
-    </div>
-
-        
-    </form>
-  
-    
+    </div>     
+    </form> 
+   
     </>
   );
 }
